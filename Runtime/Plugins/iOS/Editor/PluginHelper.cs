ed6e0fd8d3e602d59d679a4c1cdcc9d270d929a0
LujainB <44361484+LujainB@users.noreply.github.com>
2022-05-19 16:59:55 +0300

    add authenticate

--- Runtime/Plugins/iOS/Editor/PluginHelper.cs
+++ Runtime/Plugins/iOS/Editor/PluginHelper.cs
@@ -8,12 +8,14 @@ public class PluginHelper : MonoBehaviour
 
     [DllImport("__Internal")]
     private static extern int _addTwoNumberInIOS(int a, int b);
+    DllImport("__Internal")
+    private static extern string authenticate(string scheme, string redirectURI);
 
-    void Start()
+    public void authenticate()
     {
-        AddTwoNumber();
+        string token = authenticate("game1","game1://oauth-callback");
+        textResult.text = token;
     }
-
     public void AddTwoNumber()
     {
         int result = _addTwoNumberInIOS(10, 5);
